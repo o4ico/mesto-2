14f319d2012c5778de37975c01b3bd58717c91d4
ohico <o4ico@yandex.ru>
2026-06-21 17:56:21 +0300

    feat: Add field validation across all forms

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,42 +1,55 @@
 const formElement = document.querySelector('.popup__form');
 const formInput = formElement.querySelector('.popup__input');
-//const formError = formElement.querySelector(`.${formInput.id}_text-error`);
 
-// Передадим текст ошибки вторым параметром
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const formError = formElement.querySelector(`.${inputElement.id}_text-error`); //это поиск span
+  const formError = formElement.querySelector(`.${inputElement.id}_text-error`); // это поиск span
 
-  //console.log(`это объект DOM span${formError}`);
-  //console.log(`а тут должны быть его свойства? textContent ${formError.textContent}, className ${formError.className}`);
-
-  inputElement.classList.add('popup__input_error');//это добавление стиля с красной линией
-  // Заменим содержимое span с ошибкой на переданный параметр
-  formError.textContent = errorMessage;
-  formError.classList.add('popup__text-error');//это добавление класса с краснным текстом ошибки для span
+  inputElement.classList.add('popup__input_error');// это добавление стиля с красной линией
+  
+  formError.textContent = errorMessage;// заменим содержимое span с ошибкой на переданный параметр
+  formError.classList.add('popup__text-error_active');// это добавление класса с краснным текстом ошибки для span
 };
 
 const hideInputError = (formElement, inputElement) => {
-  const formError = formElement.querySelector(`.${inputElement.id}_text-error`); //это поиск span
+  const formError = formElement.querySelector(`.${inputElement.id}_text-error`); // это поиск span
 
-  inputElement.classList.remove('popup__input_error');//это удаление стиля с красной линией
+  inputElement.classList.remove('popup__input_error');// это удаление стиля с красной линией
   // Очистим ошибку
   formError.textContent = '';
-  formError.classList.remove('popup__text-error');//это удаление класса с краснным текстом ошибки для span
+  formError.classList.remove('popup__text-error_active');// это удаление класса с краснным текстом ошибки для span
 };
 
 const isValid = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
-    // Передадим сообщение об ошибке вторым аргументом
-    //console.log(`это объект validationMessage${inputElement.validationMessage}`);
 
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage);// передаем сообщение об ошибке вторым аргументом
 
-    //console.log(`это объект DOM span${formError}`);
-    //console.log(`а тут должны быть его свойства? textContent ${formError.textContent}, className ${formError.className}`);
   } else {
     hideInputError(formElement, inputElement);
   }
 };
+
+const hasInvalidTnput = (inputList) => {// проверка есть ли невалидные поля в форме
+
+  return inputList.some((inputElement) => {//если нет, то вернет false
+
+    return !inputElement.validity.valid;//если есть, вернет true
+  })
+}
+
+const toggleButtonState = (inputList, buttonElement) => {//Включение/отключение кнопки "Отправить"
+
+  if (hasInvalidTnput(inputList)) {
+
+    buttonElement.classList.add('popup__submit-button_disabled');//изменение стиля
+    buttonElement.disabled = true;//блокировка возможности нажатия
+  } else {
+
+    buttonElement.classList.remove('popup__submit-button_disabled');
+    buttonElement.disabled = false;
+  }
+} 
+
 
 formElement.addEventListener('submit', function (evt) {
   evt.preventDefault();
@@ -46,25 +59,28 @@ formElement.addEventListener('submit', function (evt) {
 //Добавление обработчиков всем полям формы
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));// сделаем массив из всех полей в форме
+  const buttonElement = formElement.querySelector('.popup__submit-button');
+
+  toggleButtonState(inputList, buttonElement);// заранее заблокировать кнопку
 
   inputList.forEach((inputElement) => {
     
     inputElement.addEventListener('input', () => {// каждому полю добавим обработчик события input
 
       isValid(formElement, inputElement);// внутри вызовем isValid передав перебираемый элемент формы
+      toggleButtonState(inputList, buttonElement);//
 
     });
-
   });
 };
 
-//Добавление обработчиков всем формам
+// Добавление обработчиков всем формам
 
 const enableValidation = () => {
 
   const formList = Array.from(document.querySelectorAll('.popup__form'));// массив из всех форм
 
-  formList.forEach((formElement) => {// каждой форме добавим setEventListeners который добавляет обработчик всем полям формы
+  formList.forEach((formElement) => {// кКаждой форме добавим setEventListeners который добавляет обработчик всем полям формы
 
     setEventListeners(formElement);
 
@@ -72,8 +88,4 @@ const enableValidation = () => {
 
 };
 
-// Вызовем функцию
 enableValidation(); 
-
-
-// Валидация работает для всех форм и полей но нужно заблокировать кнопку "сохранить", если хоть одно поле неправильное.
